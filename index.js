e439c3a4998b682e698d6f89bd5184225a12a23d
Adrianna <adrianna.bedychaj@gmail.com>
2021-01-17 17:58:12 +0000

    init

--- index.js
+++ index.js
@@ -1,56 +1,165 @@
-import { call } from "function-bind";
+// import { call } from "function-bind";
+// import { getHandler } from './utils';
 
-const PENDING = "PENDING";
-const FULFILLED = "FULFILLED";
-const REJECTED = "REJECTED";
+// const PENDING = "PENDING";
+// const FULFILLED = "FULFILLED";
+// const REJECTED = "REJECTED";
 
-export default class MyPromise {
+// export default class MyPromise {
+//   constructor(executor) {
+//     this.state = PENDING;
+//     this.queue = [];
+
+//     this.onResolve = this.onResolve.bind(this);
+//     this.fulfill = this.fulfill.bind(this);
+//     this.reject = this.reject.bind(this);
+//     this.handle = this.handle.bind(this);
+//     this.handleResolved = this.handleResolved.bind(this);
+
+
+//     this.onResolve(this,executor);
+//     this.then = function then(onFulfilled, onRejected) {
+//       this.handle(this, { onFulfilled, onRejected })
+//     };
+
+//   }
+
+//   fulfill(promise, value) {
+//     promise.state = FULFILLED;
+//     promise.value = value;
+//   }
+//   reject(promise, reason) {
+//     promise.state = REJECTED;
+//     promise.value = reason;
+//   }
+
+//   handleResolved(promise, handler) {
+//     // if (promise.state === FULFILLED) {
+//     //   return onFulfilled(promise.value);
+//     // }
+
+//     // onRejected(promise.value);
+//     const cb = promise.state === FULFILLED ? handler.onFulfilled : handler.onRejected
+//   cb(promise.value)
+//   if(promise.state === FULFILLED){
+//    return handler.onFulfilled(promise.value)
+//   } else {
+// return     handler.onRejected(promise.value)
+
+//   }
+//   }
+  
+//   handle(promise, handler) {
+//     // const handlerShape = getHandler(handler)
+//     // const {name, method} = handlerShape
+//     if (promise.state === PENDING) {
+//       promise.queue.push(handler)
+//     } else {
+//       // execute 
+//       promise.handleResolved(promise, handler)
+//     }
+//   }
+
+//   onResolve(promise, executor) {
+//     let called = false;
+//     function onFulfill(value) {
+//       if (called) return;
+//       called = true;
+//       promise.fulfill(promise, value);
+//     }
+//     function onReject(reason) {
+//       if (called) return;
+//       called = true;
+//       promise.reject(promise, reason);
+//     }
+//     try {
+//       executor(onFulfill, onReject);
+//     } catch (error) {
+//       onReject(error);
+//     }
+//   }
+
+
+// }
+// possible states
+const PENDING = 'PENDING'
+const FULFILLED = 'FULFILLED'
+const REJECTED = 'REJECTED'
+
+class MyPromise {
   constructor(executor) {
-    this.state = PENDING;
-    this.onResolve = this.onResolve.bind(this);
-    this.fulfill = this.fulfill.bind(this);
-    this.reject = this.reject.bind(this);
-    this.onResolve(this, executor);
-    this.then = function then(onFulfilled, onRejected) {
-      console.log(typeof onRejected, typeof onFulfilled);
+    this.state = PENDING
+    this.called = false
+    // value | reason = `value` 
 
-      this.handleResolved(this, onFulfilled, onRejected);
-    };
-    this.handleResolved = this.handleResolved.bind(this);
+    this.queue = []
+
+
+    // call immediately
+    this.doResolve(executor)
   }
 
-  fulfill(promise, value) {
-    promise.state = FULFILLED;
-    promise.value = value;
+  then(onFulfilled, onRejected){
+    // this.handleResolved( onFulfilled, onRejected)
+
+    this.handle({ onFulfilled, onRejected })
   }
-  reject(promise, reason) {
-    promise.state = REJECTED;
-    promise.value = reason;
+   fulfill( value){
+    this.state = FULFILLED
+    this.value = value
+    this.finale()
+
+  }
+  
+  // reject with `reason`
+   reject( reason) {
+    this.state = REJECTED
+    this.value = reason
+    this.finale()
+
+  }
+  
+  handle(handler){
+    if(this.state === PENDING){
+      return this.queue.push(handler)
+    } 
+    this.handleResolved(handler)
   }
 
-  onResolve(promise, executor) {
-    let called = false;
-    function onFulfill(value) {
-      if (called) return;
-      called = true;
-      promise.fulfill(promise, value);
-    }
-    function onReject(reason) {
-      if (called) return;
-      called = true;
-      promise.reject(promise, reason);
-    }
-    try {
-      executor(onFulfill, onReject);
-    } catch (error) {
-      onReject(error);
+  finale() {
+    const length = this.queue.length
+    for (let i = 0; i < length; i += 1) {
+      this.handle(this.queue[i])
     }
   }
 
-  handleResolved(promise, onFulfilled, onRejected) {
-    if (promise.state === FULFILLED) {
-      return onFulfilled(promise.value);
+   doResolve(executor) {
+   const onFulfilled = (value)=>{
+     if(this.called) return;
+     this.called = true
+     this.fulfill(value)
     }
-    onRejected(promise.value);
+
+    const onRejected = (reason) => {
+      if(this.called) return;
+      this.called = true
+      this.reject(reason)
+    }
+try {
+  executor(onFulfilled
+    , onRejected)
+} catch (error) {
+  onRejected(error)
+}
+  }
+
+
+  handleResolved({onFulfilled, onRejected}) {
+    this.state === FULFILLED ? onFulfilled(this.value) : onRejected(this.value)
   }
 }
+
+ 
+
+
+module.exports = MyPromise;
--- index.test.js
+++ index.test.js
@@ -102,3 +102,55 @@ it("If executor fails, promise should be REJECTED", () => {
   expect(onRejected.mock.calls[0][0]).toBe(REASON);
   expect(promise.state === "REJECTED");
 });
+
+
+it("When the promise is not fulfilled immediately, the callbacks shuld be queue", done => {
+  const promise = new MyPromise((fulfill, reject) => {
+    setTimeout(fulfill, 1, VALUE)
+  })
+  const onFulfilled = jest.fn()
+
+  promise.then(onFulfilled)
+
+  setTimeout(() => {
+    expect(onFulfilled.mock.calls.length).toBe(1);
+    expect(onFulfilled.mock.calls[0][0]).toBe(VALUE);
+  }, 5)
+
+  expect(onFulfilled.mock.calls.length).toBe(0)
+
+  setTimeout(function () {
+    // should have been called twice
+    expect(onFulfilled.mock.calls.length).toBe(2)
+    expect(onFulfilled.mock.calls[1][0]).toBe(reason)
+    done()
+  }, 10)
+
+})
+
+it('should queue callbacks when the promise is not rejected immediately', done => {
+  const reason = 'I failed :('
+  const promise = new MyPromise((fulfill, reject) => {
+    setTimeout(reject, 1, reason)
+  })
+
+  const onRejected = jest.fn()
+
+  promise.then(null, onRejected)
+  setTimeout(() => {
+    // should have been called once
+    expect(onRejected.mock.calls.length).toBe(1)
+    expect(onRejected.mock.calls[0][0]).toBe(reason)
+    promise.then(null, onRejected)
+  }, 5)
+
+  // should not be called immediately
+  expect(onRejected.mock.calls.length).toBe(0)
+
+  setTimeout(function () {
+    // should have been called twice
+    expect(onRejected.mock.calls.length).toBe(2)
+    expect(onRejected.mock.calls[1][0]).toBe(reason)
+    done()
+  }, 10)
+})
